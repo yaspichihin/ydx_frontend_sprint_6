28dfbd4aa653dceb59ac61c8411edde423987955
Yaroslav Spichihin <yaspichihin@gmail.com>
2025-10-23 02:21:56 +0300

    add search

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import { initTable } from "./components/table.js";
 import { initPagination } from "./components/pagination.js";
 import { initSorting } from "./components/sorting.js";
 import { initFiltering } from "./components/filtering.js";
+import { initSearching } from "./components/searching.js";
 
 // Исходные данные используемые в render()
 const { data, ...indexes } = initData(sourceData);
@@ -48,6 +49,7 @@ function render(action) {
   let result = [...data];
 
   // @todo: использование
+  result = applySearching(result, state, action);
   result = applyFiltering(result, state, action);
   result = applySorting(result, state, action);
   result = applyPagination(result, state, action);
@@ -59,7 +61,7 @@ const sampleTable = initTable(
   {
     tableTemplate: "table",
     rowTemplate: "row",
-    before: ["header", "filter"],
+    before: ["search", "header", "filter"],
     after: ["pagination"],
   },
   render
@@ -94,6 +96,10 @@ const applyFiltering = initFiltering(sampleTable.filter.elements, {
   // для элемента с именем searchBySeller устанавливаем массив продавцов
   searchBySeller: indexes.sellers,
 });
+
+
+const applySearching = initSearching("search");
+
 
 const appRoot = document.querySelector("#app");
 appRoot.appendChild(sampleTable.container);
